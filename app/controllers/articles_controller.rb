3fcbc078013e214ac42e47b4eff83a47331c919d
Wahyu Budi Saputra <wahyubucil@gmail.com>
2019-01-04 12:40:58 +0800

    Create comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+    http_basic_authenticate_with name: "kankusai", password: "notsecretpassword", except: [:index, :show]
+
     def index
         @articles = Article.all
     end
